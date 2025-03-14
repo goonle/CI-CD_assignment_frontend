dad7b15967fa8098a295a5bff71fbcff4e977312
goonle <junhuh741@gmail.com>
2025-03-14 21:38:25 +1300

    delete a note

--- src/component/NoteList.jsx
+++ src/component/NoteList.jsx
@@ -60,25 +60,18 @@ const NoteList = ({notes}) => {
         }
 
         const onDelete = (id) => {
-            // const token = localStorage.getItem('token');
-            // const config = {
-            //     method: 'delete',
-            //     maxBodyLength: Infinity,
-            //     url: `${SERVER_URL}/note/${id}/`,
-            //     headers: {
-            //         'Authorization': `Token ${token}`
-            //     }
-            // };
-            // if (token) {
-            //     axios.request(config)
-            //         .then((response) => {
-            //             console.log(JSON.stringify(response.data));
-            //             setNoteList(noteList.filter(note => note.id !== id));
-            //         })
-            //         .catch((error) => {
-            //             console.log(error);
-            //         });
-            // }
+            const token = localStorage.getItem('token');
+            const delConfig = {...config, method: "delete", data : {note_id : id}};
+            if (token) {
+                axios.request(delConfig)
+                    .then((response) => {
+                        console.log(JSON.stringify(response.data));
+                        setNoteList(noteList.filter(note => note.id !== id));
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
         };
 
         return (
