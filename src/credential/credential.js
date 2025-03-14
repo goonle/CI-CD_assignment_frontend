605237f67dcceee18bd1f0c5f5063a6889887a9d
goonle <junhuh741@gmail.com>
2025-03-14 21:59:10 +1300

    typo

--- src/credential/credential.js
+++ src/credential/credential.js
@@ -1,4 +1,4 @@
 // const SERVER_URL = 'http://localhost:8000';
-const SERVER_URL = 'https://ci-cd-assignment-backend-git-10-b-015e74-junhyung-huhs-projects.vercel.app/';
+const SERVER_URL = 'https://ci-cd-assignment-backend-git-10-b-015e74-junhyung-huhs-projects.vercel.app';
 
 export default SERVER_URL;
