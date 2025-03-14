8f2fedde0fed748d3152acfd647556577ff6923f
goonle <junhuh741@gmail.com>
2025-03-14 21:24:25 +1300

    update note

--- src/component/OpenModalBtn.jsx
+++ src/component/OpenModalBtn.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
 
-function AddNoteBtn(props) {
+function OpenModalBtn(props) {
     const {clickHandler, btnText} = props;
     return (
         <div className="d-flex justify-content-end mb-3">
-            <Button variant="primary" className="mb-3" onClick={clickHandler}>btnText</Button>
+            <Button variant="primary" className="mb-3" onClick={clickHandler}>{btnText}</Button>
         </div>
     );
 }
 
-export default AddNoteBtn;
+export default OpenModalBtn;
