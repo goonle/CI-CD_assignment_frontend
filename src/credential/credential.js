7e9aa9082cd059efc24c571de31435172c5b435d
goonle <junhuh741@gmail.com>
2025-03-17 15:14:48 +1300

    I think it work now

--- src/credential/credential.js
+++ src/credential/credential.js
@@ -1,6 +1,6 @@
 const LOCAL_SERVER_URL = 'http://localhost:8000';
 const REMOTE_SERVER_URL = 'https://ci-cd-assignment-backend.vercel.app';
 const TEST_SERVER_URL = 'https://ci-cd-assignment-backend-git-24-b-eeb071-junhyung-huhs-projects.vercel.app';
-const SERVER_URL = TEST_SERVER_URL;
+const SERVER_URL = REMOTE_SERVER_URL;
 
 export default SERVER_URL;
